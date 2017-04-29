8623a85362c8a2ab5525fac5172eefa420c51dc0
Carlos Eduardo López Aldana <cela1972@gmail.com>
2017-04-28 21:54:56 -0300

    Actualizacion MEF Teclado

--- MEF_tecladoMatricial/MEF_tecladoMatricial.c
+++ MEF_tecladoMatricial/MEF_tecladoMatricial.c
@@ -6,33 +6,79 @@
 
 /*==================[inlcusiones]============================================*/
 
-#include "MEF_tecladoMatricial.h"   // <= su propio archivo de cabecera
 #include "sapi.h"                     // <= Biblioteca sAPI
+#include "MEF_tecladoMatricial.h"   // <= su propio archivo de cabecera
 
 /*==================[definiciones y macros]==================================*/
 
 /*==================[definiciones de datos internos]=========================*/
+// SACAR PARA PRUEBAS...
+extern uint32_t pasos;
 
 /*==================[definiciones de datos externos]=========================*/
 
+
+uint16_t primerDigito  = 0; // Variable para almacenar el primer digito ingresado.
+uint16_t segundoDigito = 0; // Variable para almacenar el segundo digito ingresado.
+uint16_t confirmar = 0;     // Variable para almacenar si se confirma o cancela en ingreso de los datos.
+
+uint32_t indiceTeclaPresionada = 0; // Variable que me permite saber cuantas veces fue presionado el teclado y asi saber que tecla se espera.
+uint32_t indiceTeclaGuardar = 0;    // Variable que me permite conocer que digito fue ingresado para su posterior almacenamiento. (Similar a la anterior pero se usa en main).
+
+
+int almacenarPisos [10];    // Vector para almacenar hasta 10 pisos ingresados correctamente.
+int indice = 0;                  // Indice para recorrer el vector anterior.
+
+/* Vector para almacenar los pines que van a estar conectados a las filas del teclado matricial. */
+uint8_t pinesFila[4] = {
+    RS232_TXD,
+    CAN_RD,
+    CAN_TD,
+    T_COL1
+};
+
+/* Vector para almacenar los pines que van a estar conectados a las columnas del teclado matricial. */
+uint8_t pinesColumna[4] = {
+    T_FIL0,
+    T_FIL3,
+    T_FIL2,
+    T_COL0
+};
+
+static uint16_t pinesTeclado[16] = {
+                                1, 2, 3, 'A',
+                                4, 5, 6, 'B',
+                                7, 8, 9, 'C',
+                                '*', 0, '#', 'D'
+};
+
+delay_t miDelay;
+
+
 /*==================[declaraciones de funciones internas]====================*/
 
+
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
 void actualizarMEF_tecladoMatricial (void) { 
-       if (ingresarDigito) {
-            
+
+       if (ingresarDigito()) {
+	
             delay(500);
             
             switch (indiceTeclaGuardar){
                 
-                case '0':  if (pinesTeclado[segundoDigito] == 'A') {
+                case 0:  if (pinesTeclado[segundoDigito] == 'A') {
                                 guardarPisoSimple (primerDigito);
+				
                             }
                             else if (pinesTeclado[confirmar] == 'B') {
                                 cancelar();
                             }
                         break;
                             
-                case '1':   if (pinesTeclado[confirmar] == 'A' ) {
+                case 1:   if (pinesTeclado[confirmar] == 'A' ) {
                                 guardarPisoDoble (primerDigito, segundoDigito);
                             }
                 
@@ -43,7 +89,15 @@ void actualizarMEF_tecladoMatricial (void) {
             }
         }
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
 
+
+
+
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
 void configurarTeclado (void){
     
     uint8_t i = 0;  // Variable para recorrer el vector de filas y columnas.
@@ -58,7 +112,15 @@ void configurarTeclado (void){
         gpioConfig(pinesColumna[i], GPIO_INPUT_PULLUP);
     }
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
 
+
+
+
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
 bool_t ingresarDigito (void){
     
     bool_t ret = FALSE;
@@ -104,12 +166,12 @@ bool_t ingresarDigito (void){
 /* Etapa en la cual se evalua que digito se ingreso, basicamente llevando la cuenta de las veces que se presionó una tecla*/
                 switch (indiceTeclaPresionada){
                     
-                case '0':  primerDigito = i * 4 + j;
+                case 0:  primerDigito = i * 4 + j;
                            indiceTeclaPresionada = 1;
                         break;
                 
-                case '1':  segundoDigito = i * 4 + j;
-                            if (pinesTeclado[segundoDigito] == 'A' || 'B') {
+                case 1:  segundoDigito = i * 4 + j;
+                            if ((pinesTeclado[segundoDigito] == 'A') || (pinesTeclado[segundoDigito] == 'B')) {
                                 indiceTeclaPresionada = 0;
                                 indiceTeclaGuardar = 0;
                             }
@@ -118,7 +180,7 @@ bool_t ingresarDigito (void){
                             }
                         break;
                     
-                case '2':  confirmar = i * 4 + j;
+                case 2:  confirmar = i * 4 + j;
                            indiceTeclaPresionada= 0;
                            indiceTeclaGuardar = 1;
                         break;
@@ -128,7 +190,7 @@ bool_t ingresarDigito (void){
                   Formula de teclas para Teclado Matricial (Keypad)
                   de 4 filas (rows) * 4 columnas (columns)
 
-                        c0 c1 c2 c3    Si se presiona la tecla r[i] c[j]:
+                      c0 c1 c2 c3    Si se presiona la tecla r[i] c[j]:
                   r0  0  1  2  3    
                   r1  4  5  6  7    valor = (i) * cantidadDeColumnas + (j)
                   r2  8  9 10 11    
@@ -141,59 +203,78 @@ bool_t ingresarDigito (void){
     
     return ret;
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
 
 
 
-void guardarPisoSimple (int primerDigito){
-    
+
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
+void guardarPisoSimple (int primerDigito)
+{
 /*======= Funcion que almacena el piso ingresado, de un solo digito, en el vector =======*/
-
-    if (i > 0 ){
-    pinesTeclado[primerDigito] = almacenarPisos[i];
-    }
-    
-    else {
-    pinesTeclado[primerDigito] = almacenarPisos[0];
-    }
-    
-    i ++;
+if (indice < 10)
+	{
+	if (indice > 0)
+		almacenarPisos[indice] = pinesTeclado[primerDigito];
+	else	almacenarPisos[0] = pinesTeclado[primerDigito];
+	indice ++;
+	}
+primerDigito  = 0;
+segundoDigito = 0;
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
 
 
-void guardarPisoDoble (int primerDigito, int segundoDigito){
-    
-/*======= Funcion que almacena el piso ingresado, de dos digitos, en el vector =======*/
-    int carga = 0;
 
-/*======= Etapa de almacenar en la variable 'carga' el piso ingresado =======*/
-    if( pinesTeclado[segundoDigito] == 0 ){
-        carga = pinesTeclado[primerDigito] * 10;
-    }
-    
-        else {
-            carga = primerDigito + segundoDigito + 9;
-        }
 
-/*======= Etapa de carga en el vector que almacena los pisos ingresados =======*/
-    if ( i > 0 ){
-        carga = almacenarPisos[i];
-    }
-    
-        else{
-            carga = almacenarPisos[0];
-        }
-    
-    i++;
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
+void guardarPisoDoble (int primerDigito, int segundoDigito)
+{
+if (indice < 10)
+	{
+	/*======= Funcion que almacena el piso ingresado, de dos digitos, en el vector =======*/
+	int carga = 0;
+	/*======= Etapa de almacenar en la variable 'carga' el piso ingresado =======*/
+	if (pinesTeclado[segundoDigito] == 0)
+		carga = pinesTeclado[primerDigito] * 10;
+	else	carga = primerDigito + segundoDigito + 9;
+	/*======= Etapa de carga en el vector que almacena los pisos ingresados =======*/
+	if (indice > 0)
+		almacenarPisos[indice] = carga;
+	else	almacenarPisos[0] = carga;
+	indice++;
+	}
+primerDigito  = 0;
+segundoDigito = 0;
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
 
+
+
+
+//*********************************************************************************************************************
+//
+//*********************************************************************************************************************
 void cancelar (void){
     
     int borrar = 0;
     
 /* Ponemos un '0' en la última posicion almaceada*/
-    almacenarPisos [i-1] = borrar;
+    almacenarPisos [indice-1] = borrar;
     
 }
+//*********************************************************************************************************************
+//*********************************************************************************************************************
+
+
+
 /*==================[declaraciones de funciones externas]====================*/
 
 /*==================[definiciones de funciones internas]=====================*/
